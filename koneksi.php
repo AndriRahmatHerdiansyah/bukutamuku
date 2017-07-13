b2afefd7c8907e85b54bd3c85bfbb257982f5c8e
AndriRahmatHerdiansyah <bandrotandri21@gmail.com>
2017-07-13 15:30:41 +0700

    koneksi.php edited

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$server = "localhost";
-$username = "root";
-$password = "";
-$database = "bukutamu";
+$server = "ap-cdbr-azure-southeast-b.cloudapp.net";
+$username = "b2e66cc20418ca";
+$password = "4f5fbb4a";
+$database = "acsm_363d81adf7ff2e8";
 
 // Koneksi dan memilih database di server
 mysql_connect($server,$username,$password) or die("Koneksi gagal");
